9f17f7a535ba7ff438002e5557c13bf99485019a
Ole Walberg <greblavelo@gmail.com>
2021-01-24 12:42:43 +0100

    Makes three section for html, css and javascript. Makes some cheats

---
+++ jquery/loader.js
@@ -0,0 +1,14 @@
+//Loades the HTML Article
+$(document).ready(function(){
+    $("#html").load('html/html.html');
+});
+
+//Loades the CSS Article
+$(document).ready(function(){
+    $("#css").load('css/css.html');
+});
+
+//Loades the Javascript Article
+$(document).ready(function(){
+    $("#javascript").load('javascript/javascript.html');
+});
