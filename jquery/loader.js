13ec3ec66ad01053956fd64385c471f4b628bdcb
Ole Walberg <greblavelo@gmail.com>
2021-01-24 15:08:59 +0100

    done a lot of work

--- jquery/loader.js
+++ jquery/loader.js
@@ -1,14 +1,19 @@
 //Loades the HTML Article
-$(document).ready(function(){
+$(document).ready(function() {
     $("#html").load('html/html.html');
 });
 
 //Loades the CSS Article
-$(document).ready(function(){
+$(document).ready(function() {
     $("#css").load('css/css.html');
 });
 
 //Loades the Javascript Article
-$(document).ready(function(){
+$(document).ready(function() {
     $("#javascript").load('javascript/javascript.html');
+});
+
+//Loades the Resource Article
+$(document).ready(function() {
+    $('#resources').load('resources/resources.html');
 });
