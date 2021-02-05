0d1f64c5f9ab651d2f73ffa8b135c3c3cec3a1cd
Ole Walberg <greblavelo@gmail.com>
2021-02-05 12:36:49 +0100

    adds section for testing with Mocha

--- jquery/loader.js
+++ jquery/loader.js
@@ -1,24 +1,28 @@
 //Loades the HTML Article
-$(document).ready(function() {
+$(document).ready(function () {
     $("#html").load('html/html.html');
 });
 
 //Loades the CSS Article
-$(document).ready(function() {
+$(document).ready(function () {
     $("#css").load('css/css.html');
 });
 
 //Loades the Javascript Article
-$(document).ready(function() {
+$(document).ready(function () {
     $("#javascript").load('javascript/javascript.html');
 });
 
+$(document).ready(function () {
+    $("#testing").load('javascript/testing.html')
+})
+
 //Loades the Design Article
-$(document).ready(function() {
+$(document).ready(function () {
     $('#design').load('design/design.html');
 });
 
 //Loades the Resource Article
-$(document).ready(function() {
+$(document).ready(function () {
     $('#resources').load('resources/resources.html');
 });
