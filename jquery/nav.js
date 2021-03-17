9c0e04d2c9924abff9817a12f1cd5ead2fca2f0d
Ole Walberg <greblavelo@gmail.com>
2021-03-17 16:27:41 +0100

    added nav.js

--- jquery/nav.js
+++ jquery/nav.js
@@ -1,13 +1,13 @@
-const navChildren = [];
-
 const gatherChildren = () => {
+    const navChildren = [];
     document.getElementById('nav').childNodes.forEach(element => {
         element.nodeName === 'LI' ? navChildren.push({parent: element.childNodes[0], child: element.childNodes[2]}) : null;
     });
+    return navChildren;
 }
 
-const assignToggle = () => {
-    navChildren.forEach(element => {
+const assignToggle = (arr) => {
+    arr.forEach(element => {
         $(element.child).hide();
         $(element.parent).click((event) => {
             console.log(event);
@@ -17,6 +17,5 @@ const assignToggle = () => {
     });
 }
 
-gatherChildren();
-assignToggle();
+assignToggle(gatherChildren());
 
